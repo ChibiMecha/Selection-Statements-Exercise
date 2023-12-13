8ffa8ed9655b3626a439fa95732220f296fd6d2b
Vernon Hudson <vhudson@crimson.ua.edu>
2023-12-13 10:29:45 -0600

    Used do/while statement to create guessing favorite number game. Great practice for declaring and its explicit rules(can declare in one scope and intialize in another. Used if, else-if, and else statements to print messages based on input.

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -2,9 +2,31 @@
 {
     public class Program
     {
+        
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            var favNumber = r.Next(1, 10);
+            int userInput;
+            do
+            {
+                Console.WriteLine("Try to guess my favorite number 1-10");
+                userInput = int.Parse(Console.ReadLine());
+            
+                if (userInput < favNumber)
+                {
+                Console.WriteLine("Too Low");
+                }
+                else if (userInput > favNumber)
+                {
+                Console.WriteLine("Too High");
+                }
+                else
+                {
+                Console.WriteLine("You guessed it!!!");
+                }
+            } while (userInput != favNumber);
         }
     }
 }
